071a2efbac5c7c94026e840f7a5135d274c52a3d
Irving Greisman <irving.greisman@gmail.com>
2025-11-11 07:22:11 -0800

    Fix API export syntax for Vercel compatibility

--- api/complete-faqs.js
+++ api/complete-faqs.js
@@ -1,5 +1,5 @@
 // Complete submarine FAQ data - ALL 185 FAQs from markdown files
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Enable CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
